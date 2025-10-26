f5607c2ef0ea950b681eda50c505cd0a38dc899b
ShaunRainey <151593013+ShaunRainey@users.noreply.github.com>
2025-10-26 15:51:46 +0000

    Create fromDict method
    Create fromDict in order to read CSV contents into a dictionary, allowing properties to be changed with an update function

--- item.py
+++ item.py
@@ -1,11 +1,11 @@
 class InventoryItem:
-    def __init__(inventoryItem, itemId, itemName, itemQuantity, unitType, category, dateUpdated, updatedBy):
+    def __init__(self, itemId, itemName, itemQuantity, unitType, category, dateUpdated, updatedBy):
         
-        inventoryItem.itemId = str(itemId) # no validation needed, the programme is in control of this value
+        self.itemId = str(itemId) # no validation needed, the programme is in control of this value
 
         if not itemName.strip(): #ensures name is truthy
             raise ValueError("Please enter a valid string")  
-        inventoryItem.itemName = itemName.strip()
+        self.itemName = itemName.strip()
 
         try:
             quantity = int(itemQuantity) #check to see if the value is numeric. If this fails, move to except
@@ -15,24 +15,24 @@ class InventoryItem:
         except ValueError: 
             print("Quantity must be a whole number")
             return
-        inventoryItem.itemQuantity = itemQuantity.strip()
+        self.itemQuantity = itemQuantity.strip()
 
         if not unitType.strip(): #ensures unitType is truthy
             raise ValueError("Please enter a valid string")  
-        inventoryItem.unitType = unitType.strip()
+        self.unitType = unitType.strip()
 
         if not category.strip():
             raise ValueError("Please enter a valid string")
-        inventoryItem.category = category.strip()
+        self.category = category.strip()
 
-        inventoryItem.dateUpdated = dateUpdated
+        self.dateUpdated = dateUpdated
 
-        inventoryItem.updatedBy = updatedBy
+        self.updatedBy = updatedBy
 
     def __str__(inventoryItem): #This method dictates what is printed if you just print the object
         return f"{inventoryItem.itemId} {inventoryItem.itemName} {inventoryItem.itemQuantity} {inventoryItem.unitType} {inventoryItem.category} {inventoryItem.dateUpdated} {inventoryItem.updatedBy}"
     
-    def to_dict(inventoryItem): #create a dictionary object from class properties
+    def toDict(inventoryItem): #create a dictionary object from class properties
         return {
             "itemId"      : inventoryItem.itemId,
             "itemName"    : inventoryItem.itemName,
@@ -42,3 +42,14 @@ class InventoryItem:
             "dateUpdated" : inventoryItem.dateUpdated,
             "updatedBy"   : inventoryItem.updatedBy
         }
+    
+    def fromDict(cls, data): #cls = class. Effectively takes information and structures it to the class blueprint. Maintains all validation from the class
+        return cls( #this method takes a dictionary, and creates an InventoryItem object
+            itemId       = data["itemId"],
+            itemName     = data["itemName"],
+            itemQuantity = data["itemQuantity"],
+            unitType     = data["unitType"],
+            category     = data["category"],
+            dateUpdated  = data["dateUpdated"],
+            updatedBy    = data["updatedBy"]
+        )
