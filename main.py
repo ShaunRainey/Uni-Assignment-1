0e1fee51ee36764940c6de03743c7c4f0e318831
ShaunRainey <151593013+ShaunRainey@users.noreply.github.com>
2025-10-27 01:05:27 +0000

    Fix createItemObject, create deleteEntry function
    createItemObject wasn't quite working, prints were activating even if the item creation failed. Now works as intended.
    
    deleteEntry function added, does what it says on the tin

--- main.py
+++ main.py
@@ -33,27 +33,32 @@ def nextItemId(): #Allows Id to incremenet with each addition
 def promptInput(message): #helper function to avoid writing .strip() over and over
     return input(message).strip()
 
-def appendRow(row):
+def appendRow(row): #Adds a new entry to the bottom of the CSV file
     checkCSV()
     with open(CSV_Path, "a", newline="", encoding="utf-8") as f: 
         w = csv.DictWriter(f, fieldnames=fields)
         w.writerow(row)
     
-def createItemObject():
-    itemId = nextItemId()
-    name = promptInput("Item name: ")
-    quantity = promptInput("Quantity: ")
-    unit = promptInput("Unit: ")
-    category = promptInput("Category: ")
-    addedBy = promptInput("Your name: ")
-    date = datetime.now().strftime("%Y-%m-%d")
+def createItemObject(): #Creates an instance of the InventoryItem class, giving access to class methods
+    while True:
+        itemId = nextItemId()
+        name = promptInput("Item name: ")
+        quantity = promptInput("Quantity: ")
+        unit = promptInput("Unit: ")
+        category = promptInput("Category: ")
+        addedBy = promptInput("Your name: ")
+        date = datetime.now().strftime("%Y-%m-%d")
 
-    newItem = InventoryItem(itemId, name, quantity, unit, category, date, addedBy)
+        try:
+            newItem = InventoryItem(itemId, name, quantity, unit, category, date, addedBy)
 
-    print("Item Created \n")
-    print(newItem.toDict())
+            print(newItem.toDict())
+            print("Item Created \n")
 
-    return newItem.toDict()
+            return newItem.toDict()
+        
+        except ValueError as e:
+            print(f"\n Item creation failed: {e}")
 
 def AddItem():
     row = createItemObject()
@@ -128,33 +133,51 @@ def updateItem():
                 print("CSV file updated successfully \n")
             break
     else:
-        print("\n Invalid ID \n ")
+        print("\n Invalid ID \n ")    
 
-            
+def deleteEntry():
+    rows = readAll()
+    tabulateData(rows)
+    changeId = promptInput("Please specify the ID of the item you would like to delete: ")
+
+    for r in rows:
+        if r["itemId"] == changeId:
+            tabulateData([r])
+            confirm = promptInput("Are you sure you want to delete this entry? Enter 'y' to confirm.\n")
+            if confirm == 'y':
+                rows.pop(rows.index(r)) #pop removes based on an index value
+                overWriteCSV(rows)
+                print("Item deleted \n")
+            else:
+                print("Delete aborted \n")
+    
 
 def main():
     checkCSV()
     while True:
-        print("What would you like to do?")
-        print("1) Add item")
-        print("2) List items")
-        print("3) Quit")
-        print("4) test search")
-        print("5) test update")
-        choice = input("Choose (1/2/3/4/5): ").strip()
+        print("\n What would you like to do? \n")
+        print("1) List items")
+        print("2) Add item")
+        print("3) Update items")
+        print("4) Delete items")
+        print("5) Search for an item")
+        print("6) Quit")
+        choice = input("\n Choose (1/2/3/4/5/6): ").strip()
 
         match choice:
             case "1":
-                AddItem()
-            case "2":
                 listItems()
+            case "2":
+                AddItem()
             case "3":
+                updateItem()
+            case "4":
+                deleteEntry()
+            case "5":
+                searchById()
+            case "6":
                 print("Goodbye!")
                 break
-            case "4":
-                searchById()
-            case "5":
-                updateItem()
             case _: #_ is the "default" word for python
                 print("Enter a valid number.\n")
 
