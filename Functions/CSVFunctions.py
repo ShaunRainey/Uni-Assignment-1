4ef7e5423fb3c68e8270d2a3c817e52dc7f30c90
ShaunRainey <151593013+ShaunRainey@users.noreply.github.com>
2026-01-21 11:04:47 +0000

    Update CSVFunctions.py

--- Functions/CSVFunctions.py
+++ Functions/CSVFunctions.py
@@ -7,26 +7,29 @@ userFields = ["userId", "userName", "password", "role"]
 CSV_Inventory_Path = os.path.join(os.path.dirname(__file__), "../CSVFiles/inventory.csv")
 inventoryFields = ["itemId","itemName", "itemQuantity", "category", "status", "dateUpdated", "updatedBy"]
 
+# if the file doesn't exist, create it
 def checkCSV(path, headers):
     os.makedirs(os.path.dirname(path) or ".", exist_ok=True)
-    if not os.path.exists(path): # if the file doesn't exist, create it
+    if not os.path.exists(path): 
         print("Creating CSV storage file ...")
         with open(path, "w",newline="", encoding="utf-8") as f: #as f gives a name to the object returned by open(), it's effectively a file handle
             csv.writer(f).writerow(headers) #write the desired headers to the file handle
             (print("CSV storage file creation successful \n"))
 
-def readAll(path, headers): #loads the contents of csv file into a variable for use
+ #loads the contents of csv file into a variable for use
+def readAll(path, headers):
     checkCSV(path, headers)
     with open(path, "r", newline="", encoding="utf-8") as f:
         return list(csv.DictReader(f)) #creates a list containing dictionarys of the objects held in the CSV file
-    
-def nextItemId(path, headers): #Allows Id to increment with each addition
+
+#Allows Id to increment with each addition
+def nextItemId(path, headers): 
     rows = readAll(path, headers) #Rows is a list containing dictionaries
     maxId = 0
 
     for r in rows: #iterate though each row
-        itemId = r.get("itemId") #depending on the csv path, there will only be one of these 2 variables. If not using r.get, the system will crash
-        userId = r.get("userId")
+        itemId = r.get("itemId") #depending on the csv path, there will only be one of these 3 variables present. If not using r.get, the system will crash
+        userId = r.get("userId") #.get() has 2 parameters, the second is optional and is a value to return if a match isn't found. Default is None
         logId = r.get("logId")
         try:
             if(itemId):
@@ -38,14 +41,16 @@ def nextItemId(path, headers): #Allows Id to increment with each addition
         except ValueError:
             pass
     return str(maxId +1)
-    
+
+#Adds a new entry to the bottom of the CSV file
 def appendRow(row, path, headers): #Adds a new entry to the bottom of the CSV file
     checkCSV(path, headers)
     with open(path, "a", newline="", encoding="utf-8") as f: #a = append
         w = csv.DictWriter(f, fieldnames=headers) #allows you to write dictionaries directly to a csv file
         w.writerow(row)
 
-def overWriteCSV(rows, path, headers): #Allows edits to be made to the CSV file without having to delete and re-create it
+#Allows edits to be made to the CSV file without having to delete and re-create it
+def overWriteCSV(rows, path, headers):
     with open(path, "w", newline="", encoding="utf-8") as f:
         writer = csv.DictWriter(f, fieldnames=headers)
         writer.writeheader()
