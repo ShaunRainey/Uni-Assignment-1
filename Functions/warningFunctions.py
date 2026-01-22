cceb61049318f530b16d3d33d05b9016fca453ff
ShaunRainey <151593013+ShaunRainey@users.noreply.github.com>
2026-01-22 08:40:36 +0000

    Update warningFunctions.py

--- Functions/warningFunctions.py
+++ Functions/warningFunctions.py
@@ -1,6 +1,9 @@
+# Functions to create and display any warnings from the inventory CSV storage
+
 from .CSVFunctions import *
 from .utilityFunctions import tabulateData
 
+#Check the system at the time of calling for the specified issues, append any matches to a list
 def getWarnings(path, headers):
     
     warningList = []
@@ -28,12 +31,14 @@ def getWarnings(path, headers):
 
     return warningList
 
+#Displays the list created above
 def tabulateWarnings(warningList):
     
     for warning in warningList:
-        warning["messages"] = "\n".join(warning["messages"])
+        warning["messages"] = "\n".join(warning["messages"]) #Presents each issue for the same warning on a separate line for clarity
     
     tabulateData(warningList)
 
+#Prints to the console how many warnings are currently in the system
 def warningAlert(warningList):
     print(f"There are currently {len(warningList)} warnings in the system \n")
