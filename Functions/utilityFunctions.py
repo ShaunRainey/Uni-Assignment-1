40a2870c919e89a623214b01d9e45493ad80e2c3
ShaunRainey <151593013+ShaunRainey@users.noreply.github.com>
2026-01-22 08:26:49 +0000

    Update utilityFunctions.py

--- Functions/utilityFunctions.py
+++ Functions/utilityFunctions.py
@@ -1,28 +1,37 @@
+#This is a set of assistance based functions
+
 from tabulate import tabulate
 from .CSVFunctions import readAll, overWriteCSV
 
-def promptInput(message): #helper function to avoid writing .strip() over and over
+#helper function to avoid writing .strip() over and over
+def promptInput(message):
     return input(message).strip()
 
-def tabulateData(data): #Creates a table to display the CSV contents
-    try: #this is to protect against an empty csv breaking the code
+#Creates a table to display the CSV contents
+def tabulateData(data): 
+    try: #this is to protect against an empty csv breaking the program
         col_alignment = ["center"] * len(data[0])
         print(tabulate(data, headers="keys", tablefmt="grid", colalign=col_alignment) + "\n")
     except:
         print("Currently no entries held \n")
-    
-def listEntries(path, headers): #Present the CSV file to user
+
+#Present the CSV file to user
+def listEntries(path, headers):
     print("\n --- All Entries --- \n")
     rows = readAll(path, headers)
     tabulateData(rows)
-
+    
+#Delete an item from CSV storage. 
+#As this is a generic function rather than specific for user/item, it's placed in utility. If put into a more specific file as is, there will
+#be a circular dependancy/import error
+#This could be expanded to delete logs too
 def deleteEntry(path, headers):
     rows = readAll(path, headers)
     tabulateData(rows)
     
     while True:
         changeId = promptInput("Please specify the ID of the item you would like to delete: ")
-        if len(changeId) != 0:
+        if len(changeId) != 0: #empty input will abort the process
             break
         else:
             print("Invalid input, please try again")
@@ -43,7 +52,7 @@ def deleteEntry(path, headers):
                 print("Delete aborted \n")
                 break
 
-        elif userId == changeId: #to activate this, itemId has been confirmed not present, therefor userId has to be present. No truthy check needed
+        elif userId == changeId: #to activate this, itemId has been confirmed not present, therefore userId has to be present. No truthy check needed
             tabulateData([r])
             confirm = promptInput("Are you sure you want to delete this entry? Enter 'y' to confirm, else delete will be aborted.\n")
             if confirm == 'y':
